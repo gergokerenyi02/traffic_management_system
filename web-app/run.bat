0f2ce4e4d8ac70e355e8fef47a8d481c2a88c874
gergokerenyi <gergokerenyi02@gmail.com>
2025-03-27 18:08:19 +0100

    Run scripts added

---
+++ web-app/run.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Starting Spring Boot application...
+java -jar app\target\licensePlate-0.0.1-SNAPSHOT.jar
+pause
---
+++ web-app/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Starting Spring Boot application..."
+java -jar app/target/licensePlate-0.0.1-SNAPSHOT.jar
